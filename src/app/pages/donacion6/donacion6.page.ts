acf6a3791fbd73a3d121986482f7b8f4bdcb7468
ulupikita <abigail.quisbert@ucb.edu.bo>
2025-05-25 05:12:38 -0400

    backend tarjetas

--- src/app/pages/donacion6/donacion6.page.ts
+++ src/app/pages/donacion6/donacion6.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-donacion6',
@@ -7,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class Donacion6Page implements OnInit {
+  cardForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public formBuilder: FormBuilder,
+    public db: DatabaseService
+  ) {
+    this.cardForm = this.formBuilder.group({
+      numeroTarjeta: ['', [Validators.required, Validators.minLength(16)]],
+      fechaExpiracion: ['', Validators.required],
+      titularTarjeta: ['', Validators.required],
+      cvv: ['', [Validators.required, Validators.minLength(3)]],
+      monto: ['', [Validators.required, Validators.min(1)]]
+    });
   }
 
+  ngOnInit() {}
+
+  guardarTarjeta() {
+    if (this.cardForm.valid) {
+      const datos = {
+        ...this.cardForm.value,
+        tipo: 'donacion',
+        fecha: new Date()
+      };
+
+      console.log('Guardando datos del formulario:', datos);
+
+      this.db.addFirestoreDocument('tarjetas', datos)
+        .then(() => {
+          alert('Tarjeta guardada correctamente');
+          this.cardForm.reset();
+        })
+        .catch(err => {
+          console.error('Error al guardar tarjeta:', err);
+          alert('Error al guardar tarjeta: ' + err.message);
+        });
+    } else {
+      this.cardForm.markAllAsTouched();
+      alert('Formulario no válido. Por favor revisa los campos.');
+    }
+  }
 }
